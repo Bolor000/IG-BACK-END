65a953158eacdb497d4dfe4dbd49ba32175b6099
Bolor000 <bolorkhon09@gmail.com>
2025-10-04 20:00:26 +0800

    10.04

--- controller/post/get-user-posts.js
+++ controller/post/get-user-posts.js
@@ -1,9 +1,16 @@
 import { postModel } from "../../schema/post.schema.js";
+import jwt from "jsonwebtoken";
 
 export const getUserPosts = async (req, res) => {
-  const params = req.params;
-  const { userID } = params;
-  const posts = await postModel.find({ user: userID });
+  const authToken = req.headers.authorization
+
+  const accesstoken = authHeader.split('')[1]
+  console.loh(accesstoken)
+
+  const user = jwt.verify(accesstoken, "TEST");
+  console.log(user)
+
+  const posts = await postModel.find().populate("users");
 
   res.status(200).json(posts);
 };
